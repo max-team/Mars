c906d5e4d182e25514001c2fdaa53d6c3c915f64
allen-zh <winty2013@gmail.com>
2019-04-11 19:58:50 +0800

    feat: move config file parse to @marsjs/build

---
+++ packages/mars-build/src/scripts/getConfig.js
@@ -0,0 +1,82 @@
+/**
+ * @file 获取 config
+ * @author meixuguang
+ */
+
+const path = require('path');
+const fs = require('fs-extra');
+const merge = require('lodash.merge');
+const getDefaultConf = require('./defaultConfig');
+
+function getProjectConfig(options) {
+    const target = options.target || 'swan';
+
+    let projectConfig = {};
+    const configPath = path.resolve(process.cwd(), './mars.config.js');
+    if (fs.existsSync(configPath)) {
+        projectConfig = require(configPath)(target);
+    }
+
+    return projectConfig;
+}
+
+// 兼容原 Task 的配置格式
+function formatConfig(options) {
+    let config = getProjectConfig(options);
+    const {target} = options;
+    config = merge(getDefaultConf(target), config);
+
+    config.dest = {
+        path: config.dest,
+        coreDir: 'mars-core'
+    };
+    config.source = {
+        sfc: config.source,
+        assets: config.assets
+    };
+    config.options = {
+        sfc: config.options
+    };
+    config = merge(config, getRuntimeConfig(config.devConfig || {}));
+
+    // init px2units.options.multiple
+    // 规定 H5 中 1rem = 100px
+    const {designWidth, modules} = config;
+    if (designWidth && modules.postcss.px2units) {
+        const multiple = target === 'h5' ? .5 / 100 : 1;
+        modules.postcss.px2units = Object.assign({
+            targetUnits: target === 'h5' ? 'rem' : 'rpx',
+            multiple: multiple * 750 / designWidth
+        }, modules.postcss.px2units);
+    }
+    return config;
+}
+
+function getRuntimeConfig({
+    corePath = './node_modules/@marsjs/core'
+}) {
+    return {
+        source: {
+            runtime: corePath + '/src/**/*.js',
+            h5Template: path.resolve(__dirname, '../h5/template/**/*.@(vue|js|css)')
+        },
+        options: {
+            sfc: {}
+        }
+    };
+}
+
+function getConfig(options) {
+    if (!options && process.env.MARS_CLI_OPTIONS) {
+        try {
+            options = JSON.parse(process.env.MARS_CLI_OPTIONS);
+        }
+        catch(e) {}
+    }
+    if (!options) {
+        throw new Error('pass options to @marsjs/build or use @marsjs/cli');
+    }
+    return formatConfig(options);
+}
+
+module.exports = getConfig;
--- packages/mars-cli/lib/scripts/getConfig.js
+++ packages/mars-cli/lib/scripts/getConfig.js
@@ -3,38 +3,11 @@
  * @author meixuguang
  */
 
-const path = require('path');
-const fs = require('fs-extra');
-const merge = require('lodash.merge');
-
 function getConfig(cmd) {
     const target = cmd.target || 'swan';
-    const dest = (cmd && cmd.dist) || 'dist-' + target;
-
-    let projectConfig = {};
-    const configPath = path.resolve(process.cwd(), './mars.config.js');
-    if (fs.existsSync(configPath)) {
-        projectConfig = require(configPath)(target);
-    }
-
-    const config = merge({}, {
-        dest: target === 'h5' ? dest + '/src' : dest
-    }, projectConfig, target === 'h5' ? {
-        source: {
-            runtime: [],
-            assets: [
-                'src/**/*.!(vue)'
-            ]
-        }
-    } : {});
-
-    const buildPath = (config.devConfig && config.devConfig.buildPath)
-        || path.resolve(process.cwd(), 'node_modules/@marsjs/build');
 
     return {
-        config,
-        target,
-        buildPath
+        target
     };
 }
 
