fa347d0caf4e19bfbf0b9175ad0ca6a1ae6ab130
sukkee <382248373@qq.com>
2019-04-15 22:43:30 +0800

    fix(systeminfo): fix UC bug becauseOf its unusual userAgent

--- packages/mars-api/api/system/info.js
+++ packages/mars-api/api/system/info.js
@@ -12,18 +12,22 @@ function getSystemInfoSync() {
     const ua = userAgent.toLowerCase();
     let isIos = false;
     let isAndroid = false;
-    let system = undefined;
-    let platform = undefined;
-    let model = undefined;
-    const screenHeight = document.documentElement.clientHeight;
-    const screenWidth = document.documentElement.clientWidth;
-    const windowWidth = screenWidth;
-    const windowHeight = screenHeight - NAVIGATION_BAR_HEIGHT;
+    let system = '';
+    let platform = '';
+    let model = '';
+    const screenHeight = window.screen.height;
+    const screenWidth = window.screen.width;
+    const windowWidth = document.documentElement.clientWidth;
+    const windowHeight = document.documentElement.clientHeight - NAVIGATION_BAR_HEIGHT;
 
     if (userAgent.indexOf('Mac OS') > -1 || userAgent.indexOf('iPhone OS') > -1) {
         isIos = true;
     }
     else if (userAgent.indexOf('Android') > -1) {
+        isAndroid = true;
+    }
+    // 兼容UC浏览器 UA
+    else if (userAgent.indexOf('iPhone') === -1 && userAgent.indexOf('UCBrowser') > -1) {
         isAndroid = true;
     }
 
@@ -33,27 +37,27 @@ function getSystemInfoSync() {
         platform = 'ios';
 
         if (screenHeight === 736 && screenWidth === 414) {
-            model = 'iPhone6P, iPhone7P, iPhone8P';
-        }
-        else if (screenHeight === 812 && screenWidth === 375) {
-            model = 'iPhoneX';
+            model = 'iPhone 6 Plus, iPhone 7 Plus, iPhone 8 Plus';
         }
         else if (screenHeight === 667 && screenWidth === 375) {
-            model = 'iPhone6, iPhone7, iPhone8';
+            model = 'iPhone 6, iPhone 7, iPhone 8';
+        }
+        else if (screenHeight >= 812 && screenWidth >= 375) {
+            model = 'iPhone X';
         }
         else if (screenWidth > 320) {
             model = 'iPhone';
         }
         else if (screenHeight === 568 && screenWidth === 320) {
-            model = 'iPhone5, iPhoneSE, iPhone5s';
+            model = 'iPhone 5, iPhone SE, iPhone 5s';
         }
         else {
-            model = 'iPhone4s, iPhone4';
+            model = 'iPhone 4s, iPhone 4';
         }
     }
     else if (isAndroid) {
         let match = userAgent.match('Android ([0-9,.]*)');
-        system = match && match[0] || undefined;
+        system = match && match[0] || 'Android';
         platform = 'android';
         const buildIndex = ua.indexOf('build');
         const fullResult = userAgent.substring(0, buildIndex);
