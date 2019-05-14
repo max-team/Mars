f1f7ff983e9b6e41b1d806309c7358fe9d76d731
allen-zh <winty2013@gmail.com>
2019-05-14 12:02:51 +0800

    feat(build & core): add h5 runtime entry

--- packages/mars-build/src/compiler/file/compileModules.js
+++ packages/mars-build/src/compiler/file/compileModules.js
@@ -100,37 +100,22 @@ function compile(val, key, destPath) {
 async function compileUIModules(uiModules, destPath) {
     for (const key of Object.keys(uiModules)) {
         const {path: modPath, realName} = uiModules[key];
-        let coreEntry;
-        let entry;
-        let isH5 = false;
-        try {
-            coreEntry = require.resolve(realName + '/mars-core', {
-                paths: [process.cwd()]
-            });
-            entry = coreEntry.replace('mars-core/index.js', '');
-        }
-        catch (e) {
-            isH5 = true;
-            coreEntry = require.resolve(realName + '/main.js', {
-                paths: [process.cwd()]
-            });
-            entry = coreEntry.replace('/main.js', '');
-        }
-
+        const coreEntry = require.resolve(realName + '/mars-core', {
+            paths: [process.cwd()]
+        });
+        const entry = coreEntry.replace('mars-core/index.js', '');
         const dest = path.resolve(destPath, modPath);
 
         log.info('[compile:ui-module]:', getPathToCWD(entry));
         await fs.copy(entry, dest);
 
-        if (!isH5) {
-            const coreDestPath = path.resolve(destPath, 'mars-core');
-            const uiCoreDestPath = path.resolve(dest, 'mars-core');
-            const coreRelativePath = path.relative(
-                uiCoreDestPath,
-                coreDestPath
-            );
-            fs.outputFileSync(uiCoreDestPath + '/index.js', `export * from '${coreRelativePath}';`);
-        }
+        const coreDestPath = path.resolve(destPath, 'mars-core');
+        const uiCoreDestPath = path.resolve(dest, 'mars-core');
+        const coreRelativePath = path.relative(
+            uiCoreDestPath,
+            coreDestPath
+        );
+        fs.outputFileSync(uiCoreDestPath + '/index.js', `export * from '${coreRelativePath}';`);
     }
 }
 
---
+++ packages/mars-core/src/h5/index.js
@@ -0,0 +1,6 @@
+/**
+ * @file  h5 runtime entry
+ * @author zhangwentao
+ */
+
+export {default as Vue} from 'vue';
