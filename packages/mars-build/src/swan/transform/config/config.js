87669361d45b5c532178a8f6e4d27211598a5a03
Allen <winty2013@gmail.com>
2019-07-04 16:38:38 +0800

    fix(build): 支持用 use-trackby 开启 v-for 的 key 编译为 swan 的 trackBy (#128)

--- packages/mars-build/src/swan/transform/config/config.js
+++ packages/mars-build/src/swan/transform/config/config.js
@@ -6,7 +6,7 @@
 const MAP_FOR = {
     'v-for': 's-for',
     'iterator1': 's-for-index',
-    'key': 's-for-key',
+    // 'key': 's-for-key',
     'alias': 's-for-item'
 };
 
--- packages/mars-build/src/swan/transform/directive/for.js
+++ packages/mars-build/src/swan/transform/directive/for.js
@@ -14,18 +14,21 @@ module.exports = function dirFor(param, value, attrs, node) {
         iterator1,
         for: forText,
         key,
-        alias
+        alias,
+        attrsMap
     } = node;
 
     if (forText) {
-        attrs[MAP_FOR['v-for']] = `${alias || 'item'}, ${iterator1 || 'index'} in ${forText}`;
+        let forExp = `${alias || 'item'}, ${iterator1 || 'index'} in ${forText}`;
+
+        if (key && attrsMap && attrsMap.hasOwnProperty('use-trackby')) {
+            forExp += ` trackBy ${key}`;
+        }
+
+        attrs[MAP_FOR['v-for']] = forExp;
 
         if (iterator1) {
             attrs[MAP_FOR.iterator1] = iterator1;
-        }
-
-        if (key) {
-            attrs[MAP_FOR.key] = key;
         }
 
         if (alias) {
