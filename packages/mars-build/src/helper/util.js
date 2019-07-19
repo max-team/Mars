56c1c01d610d121e07cac1cc9ba8ec1722b63319
allen-zh <winty2013@gmail.com>
2019-07-19 11:59:06 +0800

    feat(build): components field support PascalCase

---
+++ packages/mars-build/src/helper/util.js
@@ -0,0 +1,44 @@
+/**
+ * @file util
+ * @author zhangwentao
+ */
+
+/**
+ * Create a cached version of a pure function.
+ */
+
+function cached(fn) {
+    const cache = Object.create(null);
+    return function cachedFn(str) {
+        const hit = cache[str];
+        return hit || (cache[str] = fn(str));
+    };
+}
+
+/**
+ * Camelize a hyphen-delimited string.
+ */
+
+const camelizeRE = /-(\w)/g;
+const camelize = cached(str => {
+    return str.replace(camelizeRE, (_, c) => c ? c.toUpperCase() : '');
+});
+
+/**
+ * Capitalize a string.
+ */
+const capitalize = cached(str => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+});
+
+/**
+ * Hyphenate a camelCase string.
+ */
+const hyphenateRE = /\B([A-Z])/g;
+const hyphenate = cached(str => {
+    return str.replace(hyphenateRE, '-$1').toLowerCase();
+});
+
+exports.camelize = camelize;
+exports.capitalize = capitalize;
+exports.hyphenate = hyphenate;
